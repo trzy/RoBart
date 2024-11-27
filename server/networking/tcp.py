d7fc5dd33949dc8fe16e14561cb31f36b4ecad99
Bart Trzynadlowski <bart.trzy@gmail.com>
2024-11-26 22:17:33 -0800

    GPL license

--- server/networking/tcp.py
+++ server/networking/tcp.py
@@ -2,6 +2,23 @@
 # tcp.py
 # Bart Trzynadlowski
 #
+# This file is part of RoBart.
+#
+# RoBart is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# RoBart is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with RoBart. If not, see <http://www.gnu.org/licenses/>.
+#
+
+#
 # Asynchronous TCP server and client. Designed to be run as asynchronous tasks in an event loop.
 #
 
