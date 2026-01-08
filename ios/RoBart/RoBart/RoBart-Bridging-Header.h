ff3e312868912263eace7ba8a339f587312d3f4c
Bart Trzynadlowski <bart.trzy@gmail.com>
2026-01-08 13:32:06 -0500

    Removed unnecessary files

--- ios/RoBart/RoBart/RoBart-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#include "FilterDepthMap.hpp"
-#include "OccupancyMap.hpp"
-#include "FindPath.hpp"
-#include "HumanInstancing.hpp"
